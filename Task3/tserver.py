5d3af373fc59985e09c3b3547822c5083ecca47f
iampkumar <iampkumar02@gmail.com>
2022-01-13 19:05:06 +0530

    correction of pause feature

--- Task3/tserver.py
+++ Task3/tserver.py
@@ -2,6 +2,7 @@ import socket
 import os
 from tqdm import tqdm
 import json
+import sys
 
 IP = "localhost"
 PORT = 4444
@@ -42,7 +43,6 @@ def get():
             json.dump(file_data, file, indent=4)
 
     data = f"{FILENAME}@{FILESIZE}"
-    # print(data)
     s.send(data.encode(FORMAT))
     msg = s.recv(SIZE).decode(FORMAT)
     print(f"SERVER: {msg}")
@@ -52,16 +52,16 @@ def get():
                unit="B", unit_scale=True, unit_divisor=SIZE)
 
     with open("E:\Computer Network\Local-torrent\server_data\share.txt", "r") as f:
-        f = open('temp_file.json')
-        data = json.load(f)
-
+        g = open('temp_file.json')
+        data = json.load(g)
+        v = 0
         for i in data['users']:
             name = i['username']
             if name == user:
                 v = i['count']
                 f.seek(v)
                 break
-
+        g.close()
         cnt1 = 0
         while True:
             data = f.read(SIZE)
@@ -71,7 +71,11 @@ def get():
             cnt += 1
             try:
                 s.send(data.encode(FORMAT))
+                bar.update(len(data))
             except:
+                print("\nClient stopped receiving!")
+                cnt -= 1
+                print(f"Total No. of Packets sended: {cnt}")
                 cnt1 = 1
                 with open('temp_file.json', 'r') as g:
                     json_data = json.load(g)
@@ -80,15 +84,13 @@ def get():
                     for i in range(0, l):
                         name = user1[i]['username']
                         if name == user:
-                            user1[i]['count'] = cnt
+                            user1[i]['count'] = v+(cnt*SIZE)
                             break
 
                     with open('temp_file.json', 'w') as g:
                         g.write(json.dumps(json_data))
                 g.close()
-
-            bar.update(len(data))
-
+                sys.exit()
         if not cnt1:
             with open('temp_file.json', 'r') as g:
                 json_data = json.load(g)
@@ -103,8 +105,8 @@ def get():
                     g.write(json.dumps(json_data))
             g.close()
 
-    print(cnt)
     s.close()
+    print(f"\nTotal No. of Packets sended: {cnt}")
 
 
 get()
