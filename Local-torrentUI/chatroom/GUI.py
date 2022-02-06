f7c89dfc0011a14a8796731b2823afe086a3fe6b
iampkumar <iampkumar02@gmail.com>
2022-02-06 09:52:47 +0530

    multiple downloads in terminal one by one

--- Local-torrentUI/chatroom/GUI.py
+++ Local-torrentUI/chatroom/GUI.py
@@ -9,6 +9,9 @@ import GetFiles.filesGUI as filesGUI
 import Pvt_Msg.pvt_msgGUI as msg_GUI
 from socket import *
 import filesocket
+from tqdm import tqdm
+import json
+import os
 
 textfont = QFont("Times", 7)
 ip_list = ["192.168.1.7"]
@@ -31,6 +34,8 @@ class Worker(QObject):
         self.user_name=input("Enter your username: ")
         myname.clear()
         myname.append(self.user_name)
+        print("MY_Name: ",myname)
+        print("TYpe: ",myname)
         self.conn.send(self.user_name.encode('utf-8'))
         self.receivingFromServer()
 
@@ -158,6 +163,94 @@ class ChatRoom(QWidget):
             down_socket=socket(AF_INET, SOCK_STREAM)
             down_socket.connect((receiver_ip, receiver_port))
             print("Connection has been established for sending file!")
+
+            FORMAT = "utf-8"
+            SIZE = 1024
+            user=receiver_name
+            FILENAME = "E:\Computer Network\Local-torrent\server_data\\share.txt"
+            FILESIZE = os.path.getsize(FILENAME)
+
+            f = open('temp_file.json')
+            data = json.load(f)
+
+            c = 0
+            for i in data['users']:
+                v = i['username']
+                if v == user:
+                    c = 1
+                    break
+            if not c:
+                j = {"username": user, "count": 0}
+
+                with open('temp_file.json', "r+") as file:
+                    file_data = json.load(file)
+                    file_data["users"].append(j)
+                    file.seek(0)
+                    json.dump(file_data, file, indent=4)
+
+            data = f"{FILENAME}@{FILESIZE}"
+            down_socket.send(data.encode(FORMAT))
+            # msg = down_socket.recv(SIZE).decode(FORMAT)
+            # print(f"SERVER: {msg}")
+            cnt = 0
+            data_size = FILESIZE/1024
+            print("FILESIZE: ", data_size)
+
+            down_socket.send(f"{data_size}".encode(FORMAT))
+            """ Data transfer. """
+            bar = tqdm(range(FILESIZE), f"Sending long.txt",
+                    unit="B", unit_scale=True, unit_divisor=SIZE)
+
+            with open("E:\Computer Network\Local-torrent\server_data\share.txt", "r") as f:
+                # data_size=len(f.read())/1024
+                # down_socket.send(f"{data_size}".encode(FORMAT))
+                # print(type(data_size))
+                # print(f"Data size: {data_size}")
+                g = open('temp_file.json')
+                data = json.load(g)
+                v = 0
+                for i in data['users']:
+                    name = i['username']
+                    if name == user:
+                        v = i['count']
+                        f.seek(v)
+                        break
+                j = 0
+                while j < v/1024:
+                    j += 1
+                    bar.update(1024)
+
+                g.close()
+                cnt1 = 0
+                cnt_size = 0
+                while True:
+                    data = f.read(SIZE)
+
+                    if not data:
+                        break
+                    cnt += 1
+                    try:
+                        cnt_size += 1
+                        down_socket.send(data.encode(FORMAT))
+                        bar.update(len(data))
+                    except:
+                        print("\nClient stopped receiving!")
+                        cnt -= 1
+                        print(f"Total No. of Packets sended: {cnt}")
+                        print("Size of file sended now: ", cnt_size/1024)
+                        cnt1 = 1
+                        cnt = v+(cnt*SIZE)
+                        temp_file_fetch(user, cnt)
+                        sys.exit()
+                if not cnt1:
+                    temp_file_fetch(user, 0)
+                # print("Size of file sended now: ", cnt_size/1024)
+
+            down_socket.close()
+            # print(f"\nTotal No. of Packets sended: {cnt}")
+            # print("total packets: sended: ", cnt_size)
+
+
         except Exception as e:
             print("Unable to connect receiver: ",e)
 
@@ -231,6 +324,22 @@ class ChatRoom(QWidget):
 
     def appendMessage(self, msg):
         self.chatWidget.append(msg)
+
+
+def temp_file_fetch(user, cnt):
+    with open('temp_file.json', 'r') as g:
+        json_data = json.load(g)
+        user1 = json_data['users']
+        l = len(user1)
+        for i in range(0, l):
+            name = user1[i]['username']
+            if name == user:
+                user1[i]['count'] = cnt
+                break
+
+        with open('temp_file.json', 'w') as g:
+            g.write(json.dumps(json_data, indent=4))
+    g.close()
 
 
 def main():
--- Local-torrentUI/filesocket.py
+++ Local-torrentUI/filesocket.py
@@ -1,6 +1,8 @@
 from socket import *
 import threading
 import chatroom.chat_client as client
+from tqdm import tqdm
+import json
 
 # socket for file transfer
 def file_socket():
@@ -15,6 +17,47 @@ def file_socket():
         
         file_conn, file_addr = server_file.accept()
         print("Connection established to download")
+        # receiving data from other peer
+
+        data = file_conn.recv(1024).decode('utf-8')
+        print("Before Error Data: ",data)
+
+        item = data.split("@")
+        FILENAME = item[0]
+        FILESIZE = int(item[1])
+
+        data_size=file_conn.recv(1024).decode('utf-8')
+        print("Data Size: ",data_size)
+
+        """ Data transfer """
+        bar = tqdm(range(
+            FILESIZE), f"Receiving long.txt", unit="B", unit_scale=True, unit_divisor=1024)
+        cnt_size = 0
+        with open("E:\Computer Network\Local-torrent\client_data\\text.txt", "w") as f:
+            g = open('E:\Computer Network\Local-Torrent\\temp_file.json')
+            data = json.load(g)
+            v = 0
+            for i in data['users']:
+                name = i['username']
+                if name == "iam":
+                    v = i['count']
+                    f.seek(v)
+                    break
+            j = 0
+            while j < v/1024:
+                j += 1
+                bar.update(1024)
+
+            g.close()
+            while True:
+                cnt_size += 1
+                data = file_conn.recv(1024).decode("utf-8")
+                if not data:
+                    break
+
+                f.write(data)
+                bar.update(len(data))
+        file_conn.close()
         
 
 file_thread= threading.Thread(target=file_socket,args=())
